25c3a6a84948a70f4d2396d9c59e8a636cef6061
rileyorear <113939466+rileyorear@users.noreply.github.com>
2023-06-22 18:15:35 -0700

    Add array of weapons and random num func

---
+++ script.js
@@ -0,0 +1,10 @@
+const weapons = ["rock", "paper", "scissors"];
+
+//random number 0-2
+function randomNum () {
+  return Math.floor(Math.random() * 3);
+}
+
+function getComputerChoice () {
+   
+}
