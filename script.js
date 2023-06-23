eff33002e53e1adeaaa93cee04c7c5e39790cfc6
rileyorear <113939466+rileyorear@users.noreply.github.com>
2023-06-23 11:31:01 -0700

    Add func bestOfFive, additional gamemode

--- script.js
+++ script.js
@@ -9,41 +9,51 @@ function getComputerChoice () {
    return weapons[randomNum()];
 }
 
+//have to refresh browser each game for accurate winner results because counters do not reset
 let playerWinCounter = 0;
 let computerWinCounter = 0;
+let message;
+let draw;
 
 function playRound (playerSelection, computerSelection) { //computerSelection should be getComputerChoice()  
-  let message;
+  
   playerSelection = playerSelection.toLowerCase();
-
   if (playerSelection === "rock" && computerSelection === "scissors") {
-    message = "You win! Rock beats scissors."
+    message = "Round win! Rock beats scissors."
     playerWinCounter++;
+    draw = false;
   } else if (playerSelection === "paper" && computerSelection === "rock") {
-    message = "You win! Paper beats rock."
+    message = "Round win! Paper beats rock."
     playerWinCounter++;
+    draw = false;
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    message = "You win! Scissors beats paper."
+    message = "Round win! Scissors beats paper."
     playerWinCounter++;
+    draw = false;
   } else if (computerSelection === "rock" && playerSelection === "scissors") {
-    message = "You lose! Rock beats scissors."
+    message = "Round loss! Rock beats scissors."
     computerWinCounter++;
+    draw = false;
   } else if (computerSelection === "paper" && playerSelection === "rock") {
-    message = "You lose! Paper beats rock."
+    message = "Round loss! Paper beats rock."
     computerWinCounter++;
+    draw = false;
   } else if (computerSelection === "scissors" && playerSelection === "paper") {
-    message = "You lose! Scissors beats paper."
+    message = "Round loss! Scissors beats paper."
     computerWinCounter++;
+    draw = false;
   } else if (computerSelection === playerSelection) {
     message = "It's a tie!";
+    draw = true;
   } else {
     message = "Please enter either 'rock', 'paper', or 'scissors'.";
+    draw = true;
   }
   return message;
 }
 
+//5 round match - no more no less, ties count as a round but do not count towards score! - typos also count as a round but do not count towards score, make sure you are accurate!
 function game () {
-  let weaponChoice;
   for (i = 0; i < 5; i++) {
     weaponChoice = prompt("What will you choose: rock, paper or scissors?", "");
     console.log(playRound (weaponChoice, getComputerChoice()));
@@ -59,4 +69,38 @@ function game () {
   }
 }
 
-game();
+//best of 5 - game doesn't stop at 3 wins if all 5 rounds haven't been played (gives possibility of the 5-0 sweep destruction) - ties are not counted.
+function bestOfFive () {
+  let gameCounter = 0;
+  let keepGoing = true;
+  while (keepGoing) {
+    if (gameCounter !== 5) {
+      weaponChoice = prompt("What will you choose: rock, paper or scissors?", "");
+      console.log(playRound (weaponChoice, getComputerChoice()));
+      if (draw === true) {
+      console.log("Player score: " + playerWinCounter);
+      console.log("Computer score: " + computerWinCounter);
+      } else {
+      console.log("Player score: " + playerWinCounter);
+      console.log("Computer score: " + computerWinCounter);
+      gameCounter++;
+      }
+    } else {
+      break;
+    }
+  }
+  if (playerWinCounter > computerWinCounter) {
+    console.log("Player wins!")
+    console.log("Player score: " + playerWinCounter);
+    console.log("Computer score: " + computerWinCounter);
+  } else if (playerWinCounter < computerWinCounter) {
+    console.log("Computer wins!")
+    console.log("Player score: " + playerWinCounter);
+    console.log("Computer score: " + computerWinCounter);
+  }
+}
+
+
+
+//game();
+//bestOfFive();
