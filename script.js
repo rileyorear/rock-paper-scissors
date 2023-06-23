dbce1a8be56e5353f0e81c848b59e414e7e76787
rileyorear <113939466+rileyorear@users.noreply.github.com>
2023-06-22 18:50:32 -0700

    Add playRound func (logic of the game)

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ function randomNum () {
 function getComputerChoice () {
    return weapons[randomNum()];
 }
+
+function playRound (playerSelection, computerSelection) { //computerSelection should be getComputerChoice()
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "You win! Rock beats scissors."
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "You win! Paper beats rock."
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "You win! Scissors beats paper."
+  } else if (computerSelection === "rock" && playerSelection === "scissors") {
+    return "You lose! Rock beats scissors."
+  } else if (computerSelection === "paper" && playerSelection === "rock") {
+    return "You lose! Paper beats rock."
+  } else if (computerSelection === "scissors" && playerSelection === "paper") {
+    return "You lose! Scissors beats paper."
+  } else {
+    return "It's a tie!";
+  }
+}
