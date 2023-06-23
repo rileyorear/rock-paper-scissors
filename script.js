519a8ac06e674b4267a2942305e28ccd124f27e0
rileyorear <113939466+rileyorear@users.noreply.github.com>
2023-06-22 19:13:44 -0700

    Add toLowerCase and invalid input support to playRound func

--- script.js
+++ script.js
@@ -9,20 +9,27 @@ function getComputerChoice () {
    return weapons[randomNum()];
 }
 
-function playRound (playerSelection, computerSelection) { //computerSelection should be getComputerChoice()
-    if (playerSelection === "rock" && computerSelection === "scissors") {
-    return "You win! Rock beats scissors."
+function playRound (playerSelection, computerSelection) { //computerSelection should be getComputerChoice()  
+  let message;
+  playerSelection = playerSelection.toLowerCase();
+
+  if (playerSelection === "rock" && computerSelection === "scissors") {
+    message = "You win! Rock beats scissors."
   } else if (playerSelection === "paper" && computerSelection === "rock") {
-    return "You win! Paper beats rock."
+    message = "You win! Paper beats rock."
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    return "You win! Scissors beats paper."
+    message = "You win! Scissors beats paper."
   } else if (computerSelection === "rock" && playerSelection === "scissors") {
-    return "You lose! Rock beats scissors."
+    message = "You lose! Rock beats scissors."
   } else if (computerSelection === "paper" && playerSelection === "rock") {
-    return "You lose! Paper beats rock."
+    message = "You lose! Paper beats rock."
   } else if (computerSelection === "scissors" && playerSelection === "paper") {
-    return "You lose! Scissors beats paper."
+    message = "You lose! Scissors beats paper."
+  } else if (computerSelection === playerSelection) {
+    message = "It's a tie!";
   } else {
-    return "It's a tie!";
+    message = "Please enter either 'rock', 'paper', or 'scissors'.";
   }
+  return message;
 }
+
